8c17337fef184f34b2f95c132b0ce4143cd46c99
yjohnson67 <joh23004@byui.edu>
2024-08-01 21:51:59 -0500

    Project and js for this as well.

--- responsiveWeb/js/script.js
+++ responsiveWeb/js/script.js
@@ -1,3 +1,4 @@
+//Sticky nav bar
 const navbar = document.querySelector('.navbar')
  
 window.addEventListener("scroll",()=>{
@@ -7,3 +8,27 @@ window.addEventListener("scroll",()=>{
         navbar.classList.remove('sticky')
     }
 })
+//End of sticky navigation bar 
+
+//Projects
+const filterLinks = document.querySelectorAll('.filter-nav-link')
+
+filterLinks.forEach((filterLink) => {
+    filterLink.addEventListener('click', (e) => {
+        e.preventDefault()
+
+        document.querySelector(".filter-nav-link.active").classList.remove("active")
+        filterLink.classList.add("active")
+
+        const project = document.querySelectorAll('.project')
+        project.forEach((project) => {
+            project.classList.add("hide")
+        })
+
+        if(filterLink.getAttribute('data-type')===project.
+            getAttribute('date-type') || filterLink.getAttribute('data-type')---'all'){
+              project.classlist.remove('hide')  
+            }
+    })
+})
+//End of Projects
