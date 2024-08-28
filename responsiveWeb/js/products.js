3c6f5417729aeebe95e2566ab6943d102c7aba3a
yjohnson67 <joh23004@byui.edu>
2024-08-27 22:56:38 -0500

    Product and Contact html.

---
+++ responsiveWeb/js/products.js
@@ -0,0 +1,10 @@
+const productsPromo = document.querySelector(".products-promo")
+const promoContent = document.querySelector(".promo-content")
+
+promoContent.addEventListener("mouseover",()=> {
+    productsPromo.classList.add("animate")
+})
+
+promoContent.addEventListener("mouseout",()=> {
+    productsPromo.classList.remove("animate")
+})
